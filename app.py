67d0cb14b172eccd670bd7f4973201714c87da5f
mattperkins <github@mattperkins.com>
2017-12-31 10:43:39 +0000

    add comments

--- app.py
+++ app.py
@@ -4,11 +4,12 @@ app = Flask(__name__)
 
 # @ signifies a decorator 
 # a way to wrap a function and modify its behaviour
+# http://localhost:5000/
 @app.route('/')
 def index():
     return "Hello, World!"
 
-
+# http://localhost:5000/about
 @app.route('/about')
 def about():
     return "<h1>About</h1>"
@@ -17,6 +18,11 @@ def about():
 @app.route('/profile/<user>')
 def profile(user):
     return "Welcome %s" % user
+
+# e.g: http://localhost:5000/post/8594
+@app.route('/post/<int:post_id>')
+def post(post_id):
+    return "Post ID = %s" % post_id
 
 
 
